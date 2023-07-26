e8320855c156c31e0e13681403968ee3700ac978
alep-2000 <alessiopiras056@gmail.com>
2023-07-26 15:12:51 +0200

    Create Migrations

---
+++ database/migrations/2023_07_26_124332_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->date('date');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->integer('train_code');
+            $table->integer('n_carriages');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
